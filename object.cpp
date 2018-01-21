933b36b52cd3aa25d0037564f886480501e58850
Marvin Reza <marvinreza@outlook.com>
2018-01-21 23:13:58 +0100

    Added cmake file and object class

---
+++ object.cpp
@@ -0,0 +1,54 @@
+#include "object.h"
+
+Object::Object(std::string name)  {
+
+	setType(name);
+	
+	if(name == "blue") {
+
+		//TODO: use "calibration mode" to find HSV min
+		//and HSV max values
+
+		setHSVmin(cv::Scalar(92,0,0));
+		setHSVmax(cv::Scalar(124,256,256));
+
+		//BGR value for Blue:
+		setColor(cv::Scalar(255,0,0));
+	}
+
+	if(name == "green") {
+
+		//TODO: use "calibration mode" to find HSV min
+		//and HSV max values
+
+		setHSVmin(cv::Scalar(34,50,50));
+		setHSVmax(cv::Scalar(80,220,200));
+
+		//BGR value for Green:
+		setColor(cv::Scalar(0,255,0));
+	}
+
+	if(name == "yellow") {
+
+		//TODO: use "calibration mode" to find HSV min
+		//and HSV max values
+
+		setHSVmin(cv::Scalar(20,124,123));
+		setHSVmax(cv::Scalar(30,256,256));
+
+		//BGR value for Yellow:
+		setColor(cv::Scalar(0,255,255));
+
+	}
+
+	if(name == "red") {
+		//TODO: use "calibration mode" to find HSV min
+		//and HSV max values
+
+		setHSVmin(cv::Scalar(0,200,0));
+		setHSVmax(cv::Scalar(19,255,255));
+
+		//BGR value for Red:
+		setColor(cv::Scalar(0,0,255));
+	}
+}
---
+++ object.h
@@ -0,0 +1,39 @@
+#ifndef OBJECT_H
+#define  OBJECT_H
+
+#include <string>
+
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+class Object {
+public:
+	Object() : type_("Object"), color_(0,0,0) { }
+	explicit Object(std::string name);
+
+	~Object() = default;
+
+    //getters
+	int             getXPos() const { return x_pos_; }
+	int             getYPos() const { return y_pos_; }
+    cv::Scalar      getHSVmin() const { return hsv_min_; }
+    cv::Scalar      getHSVmax() const { return hsv_max_; }
+    std::string     getType() const { return type_; }
+    cv::Scalar      getColor() const { return color_; }
+
+    //setters
+	void setXPos(int x) { x_pos_ = x; }
+	void setYPos(int y) { y_pos_ = y; }
+	void setHSVmin(cv::Scalar min) { hsv_min_ = min; }
+	void setHSVmax(cv::Scalar max) { hsv_max_ = max; }
+	void setType(std::string t) { type_ = t; }
+	void setColor(cv::Scalar c) { color_ = c; }
+
+private:
+	int x_pos_, y_pos_;
+    std::string type_;
+    cv::Scalar hsv_min_, hsv_max_;
+    cv::Scalar color_;
+};
+
+#endif
